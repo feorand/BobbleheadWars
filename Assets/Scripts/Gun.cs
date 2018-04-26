9cb32306e291950dd4b3eb1092e2b4168db568c8
Pavel Prokofiev <feorand@gmail.com>
2018-04-26 18:26:21 +0500

    implement firing bullets

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("FireBullet"))
+            {
+                InvokeRepeating("FireBullet", 0f, 0.1f);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("FireBullet");
+        }
+	}
+
+    private void FireBullet() {
+        var bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = launchPosition.position;
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+}
