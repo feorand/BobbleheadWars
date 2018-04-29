d4c198fff9c362ce37ed5073a17b7bb8f776eee5
Pavel Prokofiev <feorand@gmail.com>
2018-04-29 11:23:04 +0500

    added looking in a direction of the mouse pointer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,10 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed = 50.0f;
     public Rigidbody head;
+    public LayerMask layerMask;
 
     private CharacterController characterController;
+    private Vector3 currentLookTarget = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
@@ -32,5 +34,22 @@ public class PlayerController : MonoBehaviour {
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+
+            var targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            var rotation = Quaternion.LookRotation(targetPosition - transform.position);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10.0f);
+        }
     }
 }
+
